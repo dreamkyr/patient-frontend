9b1a7e88ab9dcc23a6e2f1b5483a784aff2b6c02
thomasgit123 <thomasbox123@gmail.com>
2021-10-10 21:49:44 -0400

    feat: update phone field validation message

--- src/PhoneInput.js
+++ src/PhoneInput.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-export const PhoneInput = ({ phone, setPhone }) => {
+export const PhoneInput = () => {
+  const onChange = (e) => {
+    let date = new Date(Date.parse(e.target.value));
+    
+    if (date > new Date()) {
+      e.target.setCustomValidity("请填写此字段。"); 
+    } else {
+      e.target.setCustomValidity("");
+    }
+  }
   return (
     <div className="flex flex-col justify-center">
       <p className="font-medium text-lg mb-2">手机:</p>
       <input
-        type="tel"
+        type="text"
         name="phoneNumber"
         required
-        onInvalid={(e)=>{e.target.setCustomValidity('请填写此字段。')}}
-        oninvalid="this.setCustomValidity('请填写此字段。')"
+        onChange={onChange}
+        onInvalid={(e) => {
+          e.target.setCustomValidity("请填写此字段。");
+        }}
         className="p-2 w-full border outline-none rounded border-blue-500"
       />
     </div>
